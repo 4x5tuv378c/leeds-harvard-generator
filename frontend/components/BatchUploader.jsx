23189c2396efefced29249a6dc7de15fdf375da7
4x5tuv378c <robertrwood@me.com>
2025-04-07 17:55:36 +0100

    Add files via upload

---
+++ frontend/components/BatchUploader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BatchUploader() {
+  return (
+    <div>
+      <h2>Batch Upload (CSV)</h2>
+      <input type="file" accept=".csv" />
+    </div>
+  );
+}
