2a5302887b6a7577c5fd302edc42a4fdb5eaaea9
4x5tuv378c <robertrwood@me.com>
2025-04-07 18:09:53 +0100

    Delete backend directory

--- backend/styleRules.js
+++
@@ -1,7 +0,0 @@
-const STYLE_RULES = {
-  italic: (text) => `<span style="font-style: italic;">${text}</span>`,
-  bold: (text) => `<span style="font-weight: bold;">${text}</span>`,
-  normal: (text) => `<span style="font-style: normal;">${text}</span>`
-};
-
-module.exports = STYLE_RULES;
